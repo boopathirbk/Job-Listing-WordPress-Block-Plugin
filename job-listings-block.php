83a864d5ebbd372ff4d6fb8b1a87972bc9de0ce7
Boopathi R <rbkrbk84@gmail.com>
2025-04-17 14:42:59 +0530

    Initial commit of Job Listings Block plugin

--- job-listings-block.php
+++ job-listings-block.php
@@ -5,7 +5,7 @@
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Version:           1.0.0
- * Author:            Your Name / AI Assistant
+ * Author:            Boopathi R
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       job-listings-block
